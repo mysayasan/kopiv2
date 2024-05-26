9be3708bfe4bb33c5e66b9bc67ce438f7cdeeb25
Muhammad Adib <mysayasan@gmail.com>
2024-05-26 17:56:11 +0800

    Add user module phase 1

---
+++ domain/enums/data/data.go
@@ -0,0 +1,38 @@
+package data
+
+// Paging
+type Paging struct {
+	Limit  uint64 `json:"imit" validate:"required"`
+	Offset uint64 `json:"offset" validate:"required"`
+}
+
+// Filter
+type Filter struct {
+	FieldName string      `json:"fieldName" validate:"required"`
+	Compare   Compare     `json:"compare" validate:"required"`
+	Value     interface{} `json:"value" validate:"required"`
+}
+
+type Compare int
+
+const (
+	Equal                Compare = iota + 1 // EnumIndex = 1
+	NotEqual                                // EnumIndex = 2
+	GreaterThan                             // EnumIndex = 3
+	LessThan                                // EnumIndex = 4
+	GreaterThanOrEqualTo                    // EnumIndex = 5
+	LessThanOrEqualTo                       // EnumIndex = 6
+)
+
+// Sorter
+type Sorter struct {
+	FieldName string `json:"fieldName" validate:"required"`
+	Sort      Sort   `json:"sort" validate:"required"`
+}
+
+type Sort int
+
+const (
+	ASC  Sort = iota + 1 // EnumIndex = 1
+	DESC                 // EnumIndex = 2
+)
